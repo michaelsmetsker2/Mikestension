c96d55810532dec5ffc11fc642a7cbcfcaa41ad4
michaelsmetsker2 <michaelsmetsker2@gmail.com>
2025-05-02 16:23:22 -0700

    added a hotkey for opening the side panel

---
+++ src/js/background.js
@@ -0,0 +1,21 @@
+chrome.runtime.onInstalled.addListener(() => { //will this actually work consistantly if not just oninstalled?
+  console.log('Extension installed, commands should be available');
+});
+
+chrome.commands.onCommand.addListener(() => {
+  console.log("opening side panel");
+  chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+    chrome.sidePanel.open({ windowId: tab.windowId });
+  });
+});
+
+console.log('Background script loaded, listening for commands');
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+/*
+chrome.sidePanel
+.setPanelBehavior({ openPanelOnActionClick: true })
+.catch((error) => console.error(error)
+);
+
+*/
