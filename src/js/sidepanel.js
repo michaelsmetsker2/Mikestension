8144c8e4f4b5174fd51bbdab17c6914f908039ff
michaelsmetsker2 <michaelsmetsker2@gmail.com>
2025-05-07 16:46:40 -0700

    fixed file paths

--- src/js/sidepanel.js
+++ src/js/sidepanel.js
@@ -1,15 +1,11 @@
-const tabs = await chrome.tabs.query({
-    url: [
-      "https://developer.chrome.com/docs/webstore/*",
-      "https://developer.chrome.com/docs/extensions/*",
-    ]
-});
+const tabs = await chrome.tabs.query({});
 
 const collator = new Intl.Collator();
 tabs.sort((a, b) => collator.compare(a.title, b.title));
 
 const template = document.getElementById("li_template");
 const elements = new Set();
+
 for (const tab of tabs) {
   const element = template.content.firstElementChild.cloneNode(true);
 
